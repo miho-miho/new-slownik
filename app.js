7252cea1ae0028ee926a854cd75adaf75d73c0d8
mihomiho <miho.021431.music@gmail.com>
2020-08-30 16:29:11 +0900

    コード移動

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+// .env から設定を読み込む
+require('dotenv').config();
+const env = process.env;
+
+app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME
+});
+
+app.use(express.urlencoded({
+  extended: false
+}));
+
+//ホームの表示
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+// 検索前画面
+app.get('/table', (req, res) => {
+  res.render('table.ejs', {
+    rsMeta: [],
+    rsWordlist: [],
+  });
+});
+
+// 検索結果画面
+app.post('/table', (req, res) => {
+  connection.query('SELECT * FROM info WHERE headword = ? LIMIT 1',
+    [req.body.wordItem],
+    function (err, rows, fields) {
+
+      if (err) {
+        console.log('err(meta): ' + err);
+      }
+
+      const rsMeta = rows;
+
+      connection.query('SELECT * FROM info WHERE headword = ? LIMIT 1',
+        [req.body.wordItem],
+        function (err, rows, fields) {
+
+          if (err) {
+            console.log('err(wordlist): ' + err);
+          }
+          const rsWordlist = rows;
+
+          res.render('table.ejs', {
+            rsMeta: rsMeta,
+            rsWordlist: rsWordlist,
+          });
+
+        });
+
+    });
+});
+
+
+app.listen(3000);
